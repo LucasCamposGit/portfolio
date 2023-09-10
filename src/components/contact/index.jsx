e0ef34b9d3e611e8f6dcb2653791d8290cff6b5a
LucasCamposGit <lucascpn2501@gmail.com>
2023-09-10 11:59:03 -0300

    updates

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,13 +1,28 @@
+import { useRef } from "react";
 import "./index.scss";
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const form = useRef(null);
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_22x1k0x', 'template_q6xlj9a', form.current, 'v_ig8eJD3GabxCFAs')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+    };
     return (
         <div className="contact" id="contact">
             <div className="container">
                 <h1>Contact Me</h1>
                 <p>Get in touch</p>
 
-                <form id="contact-form">
+                <form id="contact-form" ref={form} onSubmit={sendEmail}>
                     <input type="hidden" name="contact_number" />
                     <label>Name</label>
                     <input type="text" name="user_name" />
