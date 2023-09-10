5ed82683ba3d27bd1909504dc9f3bacce97f24a3
LucasCamposGit <lucascpn2501@gmail.com>
2023-09-10 14:52:12 -0300

    composition

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -2,72 +2,73 @@ import "./index.scss";
 import Brackets from "../../assets/svg/brackets.svg";
 import Down from "../../assets/svg/down-arrow.svg";
 
-export default function Skills() {
+function ListOfSkills({ skills }) {
+
+    return(
+        <div className="container-skills">
+            {skills.map((skill) => {
+                return(<li>{skill}</li>);
+            })}
+        </div>
+    )
+}
+
+function Skill({ title, listOfSkills }) {
 
     function handleClick(e) {
         let ul = e.target.nextSibling;
         let img  = e.target.lastChild;
-        console.log(img);
 
-
-        e.stopPropagation(); 
-        e.preventDefault();
         if (ul.style.height === "0px" || ul.style.height === "") {
             ul.style.height = "15vw"
             img.style.transform = "rotate(180deg)"
 
         } else {
             ul.style.height = "0px";
-        img.style.transform = "rotate(0deg)"
+            img.style.transform = "rotate(0deg)"
 
         }
 
     }
 
+    return (
+        <div className="skill">
+            <h1 onClick={handleClick}>
+                <img src={Brackets} className="brackets" alt="" />
+                {title}
+                <img src={Down} className="down" alt="" />
+            </h1>
 
+            {listOfSkills}
+        </div>
+    );
+}
+
+let frontendSkills = ["HTML", "CSS", "SASS", "JAVASCRIPT", "REACTJS"];
+let backendSkills = ["PHP (POO/MVC)"];
+let databaseSkills = ["MYSQL"];
+
+export default function Skills() {
+
+  
     return(
         <div className="skills" id="skills">
 
-            <div className="skill" >
-                <h1 onClick={handleClick}>
-                    <img src={Brackets} className="brackets" alt="" />
-                    Frontend Developer
-                    <img src={Down} className="down" alt="" />
-                </h1>
+            <Skill 
+                title={"Frontend Developer"}
+                listOfSkills={<ListOfSkills skills={frontendSkills}/>}
+            />
 
-                <ul className="container-skills">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>SASS</li>
-                    <li>JAVASCRIPT</li>
-                    <li>REACTJS</li>
-                </ul>
-            </div>
+            <Skill 
+                title={"Backend Developer"}
+                listOfSkills={<ListOfSkills skills={backendSkills}/>}
+            />
 
-            <div className="skill" >
-                <h1 onClick={handleClick}>
-                    <img src={Brackets} className="brackets" alt="" />
-                    Backend Developer
-                    <img src={Down} className="down" alt="" />
-                </h1>
+            <Skill 
+                title={"Database Developer"}
+                listOfSkills={<ListOfSkills skills={databaseSkills}/>}
+            />
 
-                <ul className="container-skills">
-                    <li>PHP (POO/MVC) </li>
-                    <li>DJANGO</li>
-                </ul>
-            </div>
-
-            <div className="skill" >
-                <h1 onClick={handleClick}>
-                    <img src={Brackets} className="brackets" alt="" />
-                    Database Developer
-                    <img src={Down} className="down" alt="" />
-                </h1>
-
-                <ul className="container-skills">
-                    <li>MYSQL</li>
-                </ul>
-            </div>
         </div>
     );
 }
