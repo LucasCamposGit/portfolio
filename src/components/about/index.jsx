c0c5fdd722d7f6a6dae1521dd204a5676dc5c1e3
LucasCamposGit <lucascpn2501@gmail.com>
2023-09-10 12:07:21 -0300

    about

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -7,16 +7,12 @@ export default function About() {
                 <h1>About Us</h1>
                 <h2>Introduction</h2>
 
-                <div className="text">
-                    <p>
-                    We are seasoned Full Stack Web Developers with two years of experience. Committed hard workers, we blend frontend and backend skills to craft efficient, visually pleasing web solutions. Our dedication to staying updated ensures cutting-edge results.
-                    </p>
-                    <p>
-                        Hard work is our hallmark. We invest our dedication and diligence into every project, ensuring efficient, scalable, and visually appealing web solutions. Our continuous learning mindset keeps us at the forefront of industry trends, enabling us to deliver exceptional digital experiences.
-                    </p>
-                    <p>
-                    Let's create remarkable web experiences together.
-                    </p>
+                <div className="text">  
+                    <p>Hello, I'm Lucas. I'm a web developer from Brazil.</p>
+
+                    <p>In 2021, I entered the world of web development, starting with simple projects. </p>
+                    <p>I'm currently pursuing a degree in Computer Science and dedicating myself to mastering web development every day. </p>
+                    <p>My enthusiasm for programming and development in general continues to grow stronger.</p>
                 </div>
 
                 <div className="experiences">
